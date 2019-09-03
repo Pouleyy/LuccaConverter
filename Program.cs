dff24d8f8129c0de572a097ee42bcbfc6ebcaccb
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-09-03 23:49:23 +0200

    :sparkles: Get info from the test file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace LuccaConverter {
     class Program {
@@ -15,9 +17,26 @@ namespace LuccaConverter {
             var fs = new FileStream (pathToFile, FileMode.Open);
             using (StreamReader reader = new StreamReader (fs)) {
                 string line = String.Empty;
+                var count = 0;
+                var listCurrencyWithDouble = new List<string> ();
                 while ((line = reader.ReadLine ()) != null) {
                     var lineContent = line.Split (";");
+                    if (count == 0) {
+                        startingCurrency = lineContent[0];
+                        endingCurrency = lineContent[2];
+                        amountToChange = Convert.ToDouble (lineContent[1]);
+                    }
+                    if (count > 1) {
+                        var currency1 = lineContent[0];
+                        var currency2 = lineContent[1];
+                        var rate = Convert.ToDouble (lineContent[2]);
+                        listCurrencyWithDouble.Add (currency1);
+                        listCurrencyWithDouble.Add (currency2);
+                        infoConversion.Add ((currency1, currency2, rate));
+                    }
+                    count++;
                 }
+                listCurrency = new HashSet<String> (listCurrencyWithDouble).ToList ();
             }
         }
     }
