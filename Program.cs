117f16a78d7552511fbe99f0ef4969414b052821
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-09-03 20:10:32 +0200

    :sparkles: Ability to read and parse the test file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
 
-namespace LuccaConverter
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace LuccaConverter {
+    class Program {
+        static void Main (string[] args) {
+            if (args[0] != null) {
+                StreamFile (args[0]);
+            } else {
+                Console.WriteLine ("Error, you need to specify a path to the file");
+            }
+        }
+
+        static void StreamFile (string pathToFile) {
+            var fs = new FileStream (pathToFile, FileMode.Open);
+            using (StreamReader reader = new StreamReader (fs)) {
+                string line = String.Empty;
+                while ((line = reader.ReadLine ()) != null) {
+                    var lineContent = line.Split (";");
+                }
+            }
         }
     }
 }
