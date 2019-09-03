f77687f1ca372755569774024fc64245d1042a56
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-09-03 23:50:02 +0200

    :boom: My own graph

--- Program.cs
+++ Program.cs
@@ -5,6 +5,35 @@ using System.Linq;
 
 namespace LuccaConverter {
     class Program {
+        public class MyGraph<T> {
+            public MyGraph () { }
+            public MyGraph (IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges) {
+                foreach (var vertex in vertices)
+                    AddVertex (vertex);
+
+                foreach (var edge in edges)
+                    AddEdge (edge);
+            }
+
+            public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>> ();
+
+            public void AddVertex (T vertex) {
+                AdjacencyList[vertex] = new HashSet<T> ();
+            }
+
+            public void AddEdge (Tuple<T, T> edge) {
+                if (AdjacencyList.ContainsKey (edge.Item1) && AdjacencyList.ContainsKey (edge.Item2)) {
+                    AdjacencyList[edge.Item1].Add (edge.Item2);
+                    AdjacencyList[edge.Item2].Add (edge.Item1);
+                }
+            }
+        }
+        private static string startingCurrency;
+        private static string endingCurrency;
+        private static double amountToChange;
+        private static List < (string, string, double) > infoConversion = new List < (string, string, double) > (); //edges of the graph
+        private static List<String> listCurrency = new List<string> (); //vertices of the graph
+
         static void Main (string[] args) {
             if (args[0] != null) {
                 StreamFile (args[0]);
